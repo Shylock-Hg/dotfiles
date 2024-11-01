5e04a363bb106b73158184c80e087a79c1d65455
Shylock Hg <tcath2s@icloud.com>
2024-11-01 11:55:06 +0800

    Add sh to install opensuse vscode repo.

--- setup-ms-repo.opensuse.sh
+++ setup-ms-repo.opensuse.sh
@@ -1,3 +1,10 @@
+#! /usr/bin/env bash
+
+# edge
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo zypper ar https://packages.microsoft.com/yumrepos/edge microsoft-edge
+# vsocode
+zypper ar -cf https://download.opensuse.org/repositories/devel:/tools:/ide:/vscode/openSUSE_Tumbleweed devel_tools_ide_vscode
+
 sudo zypper refresh
+
