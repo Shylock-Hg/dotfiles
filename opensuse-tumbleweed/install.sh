3a2660e27afcb5c8d20129604418f9a2c6033a43
Shylock Hg <tcath2s@icloud.com>
2024-11-12 10:23:16 +0800

    Install more packages.

--- opensuse-tumbleweed/install.sh
+++ opensuse-tumbleweed/install.sh
@@ -2,7 +2,8 @@
 
 sudo zypper dup -yl
 
-sudo zypper install -yl vim gcc gcc-c++ make cmake git powerline-fonts ctags \
+sudo zypper install -yl vim gcc gcc-c++ make cmake git powerline-fonts ctags clang \
     microsoft-edge-stable code \
     alacritty \
-    google-noto-sans-cjk-fonts
+    google-noto-sans-cjk-fonts \
+    fcitx5 fcitx5-configtool
