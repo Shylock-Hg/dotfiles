e5fea73db1abfda50e779df64d6452c6b2613a63
Shylock Hg <tcath2s@icloud.com>
2024-11-12 14:00:27 +0800

    Fix vscode repo.

--- opensuse-tumbleweed/setup-ms-repo.tumbleweed.sh
+++ opensuse-tumbleweed/setup-ms-repo.tumbleweed.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 
-# edge
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+# edge
 sudo zypper ar https://packages.microsoft.com/yumrepos/edge microsoft-edge
 # vsocode
-sudo zypper ar https://download.opensuse.org/repositories/devel:/tools:/ide:/vscode/openSUSE_Tumbleweed devel_tools_ide_vscode
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" |sudo tee /etc/zypp/repos.d/vscode.repo > /dev/null
 
 sudo zypper --gpg-auto-import-keys refresh
 
