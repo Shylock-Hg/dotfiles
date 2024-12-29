adabc134a64ce93db05e1d1243fff002817a2c9f
Shylock Hg <tcath2s@icloud.com>
2024-12-29 11:15:31 +0800

    Install bottles by flatpak.

--- opensuse-tumbleweed/install.sh
+++ opensuse-tumbleweed/install.sh
@@ -8,7 +8,6 @@ until sudo zypper install -yl vim gcc gcc-c++ make cmake git powerline-fonts cta
     google-noto-sans-cjk-fonts \
     fcitx5 fcitx5-configtool \
     jetbrains-mono-fonts \
-    bottles \
     ripgrep
 do
     echo 'Try again'
