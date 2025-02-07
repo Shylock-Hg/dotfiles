61fc70104f887aaca568943896cf766a4ca11d28
Shylock Hg <tcath2s@icloud.com>
2025-02-07 10:30:07 +0800

    sync.

--- sh/sync-cpa.sh
+++ sh/sync-cpa.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
-FOLDER=Workspace/cpa; while inotifywait -r -e modify,create,delete $FOLDER; do rclone sync --exclude build/ --exclude .cache/ $FOLDER dropbox:/$FOLDER --transfers 4; sleep 300; done
+FOLDER=Workspace/cpa; while inotifywait -r -e modify,create,delete $FOLDER; do sleep 300; rclone sync --exclude build/ --exclude .cache/ $FOLDER dropbox:/$FOLDER --transfers 4; done
 
--- sh/sync-tools.sh
+++ sh/sync-tools.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
-FOLDER=Tools; while inotifywait -r -e modify,create,delete $FOLDER; do rclone sync $FOLDER gdrive:/$FOLDER --transfers 4; sleep 300; done
+FOLDER=Tools; while inotifywait -r -e modify,create,delete $FOLDER; do sleep 300; rclone sync $FOLDER gdrive:/$FOLDER --transfers 4; done
 
