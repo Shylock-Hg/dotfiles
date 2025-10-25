40f6e273ebbfecec2352fc04a2a072173af411a2
Shylock Hg <tcath2s@icloud.com>
2025-10-25 18:33:31 +0800

    feat: add reverse flag to compute ratio.

--- utils/rate.py
+++ utils/rate.py
@@ -11,8 +11,13 @@ if __name__ == "__main__":
     parser.add_argument("lo", type=float)
     parser.add_argument("hi", type=float)
     parser.add_argument("--ratio", type=float)
+    parser.add_argument("--reverse", action="store_true")
     args = parser.parse_args()
 
+    # ratio 0.382 0.314 0.2718
     ratio = args.ratio if args.ratio is not None else 0.618
+    if args.reverse:
+        assert(ratio <= 1)
+        ratio = 1-ratio
 
     print(round(rate(args.lo, args.hi, ratio), 2))
