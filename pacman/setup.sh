98a7db7f110e455c33f8c80a1be4b372d7082ad8
Shylock Hg <tcath2s@icloud.com>
2025-11-02 22:07:02 +0800

    feat: pacman

---
+++ pacman/setup.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+sudo pacman -Syu
+
+sudo pacman -S --noconfirm code yay mold ninja make cmake gcc clang \
+    libc++ libc++abi \
+    ripgrep podman \
+    rclone inotify-tools \
+    flatpak \
+    fcitx5 fcitx5-configtool fcitx5-chinese-addons fcitx5-chinese-rime \
+    ttf-jetbrains-mono noto-fonts noto-fonts-cjk \
+    remmina tailscale \
+    steam
+
+sudo yay -S microsoft-edge-stable-bin bottles \
+    pgyvisitor
