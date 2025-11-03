98ac25f3a4c41f1c9faf7fd46fbadde2716959aa
Shylock Hg <tcath2s@icloud.com>
2025-11-03 10:01:06 +0800

    utils

--- utils/rate.py
+++ utils/rate.py
@@ -12,12 +12,23 @@ if __name__ == "__main__":
     parser.add_argument("hi", type=float)
     parser.add_argument("--ratio", type=float)
     parser.add_argument("--reverse", action="store_true")
+
+    parser.add_argument('--type', type=str)
     args = parser.parse_args()
 
-    # ratio 0.382 0.314 0.2718
-    ratio = args.ratio if args.ratio is not None else 0.618
-    if args.reverse:
-        assert(ratio <= 1)
-        ratio = 1-ratio
+    def getRatio():
+        return args.ratio if args.ratio is not None else 0.618
 
-    print(round(rate(args.lo, args.hi, ratio), 2))
+    if args.type is None:
+        # ratio 0.382 0.314 0.2718
+        ratio = getRatio()
+        if args.reverse:
+            assert(ratio <= 1)
+            ratio = 1-ratio
+        print("Range value: {}", round(rate(args.lo, args.hi, ratio), 2))
+    elif args.type == 'down':
+        ratio = args.ratio if args.ratio is not None else 0.0618
+        sell = round(args.hi - (args.hi * ratio), 2)
+        print("Down value: {}", sell)
+        print("Earn: ", (sell - args.lo) / args.lo * 100, "%")
+        
