b066f22cb8adbbad662116e29f579c7226909511
Shylock Hg <tcath2s@icloud.com>
2025-11-03 16:48:54 +0800

    feat: pacman install more packages.

--- pacman/setup.sh
+++ pacman/setup.sh
@@ -10,7 +10,8 @@ sudo pacman -S --noconfirm code yay mold ninja make cmake gcc clang \
     fcitx5 fcitx5-configtool fcitx5-chinese-addons fcitx5-chinese-rime \
     ttf-jetbrains-mono noto-fonts noto-fonts-cjk \
     remmina tailscale \
-    steam
+    steam \
+    wine wine-mono
 
 sudo yay -S microsoft-edge-stable-bin bottles \
     pgyvisitor
