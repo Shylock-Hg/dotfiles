5b8e5c3e9c6547ae00b9f649ab706ef644d290f3
Shylock Hg <tcath2s@icloud.com>
2025-11-10 09:42:01 +0800

    feat: install winetricks in pacman

--- pacman/setup.sh
+++ pacman/setup.sh
@@ -11,7 +11,7 @@ sudo pacman -S --noconfirm --needed code yay mold ninja make cmake gcc clang \
     ttf-jetbrains-mono noto-fonts noto-fonts-cjk \
     remmina freerdp tailscale \
     steam \
-    wine wine-mono \
+    wine wine-mono winetricks \
     cronie powertop
 
 yay -S microsoft-edge-stable-bin \
