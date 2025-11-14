7e9fae09a1a1f09c022737801639e02cb0e9fc79
Shylock Hg <tcath2s@icloud.com>
2025-11-14 15:33:28 +0800

    feat: use stow

--- wine/setup.sh
+++ wine/setup.sh
@@ -7,6 +7,6 @@ winetricks cjkfonts
 wget https://dldir1.qq.com/wework/work_weixin/WeCom_5.0.0.8002.exe -O ~/Downloads/WeCom_5.0.0.8002.exe
 wine ~/Downloads/WeCom_5.0.0.8002.exe
 
-readonly SH_P=/home/$USER/.wine/drive_c/users/$USER/Documents
-rm $SH_P  && mkdir $SH_P
+readonly SH_P=~/.wine/drive_c/users/$USER/Documents
+rm -rf $SH_P  && mkdir $SH_P
 
