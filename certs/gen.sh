65d18c2d8f5e53c13cdba697a569ff0b118c25e0
Shylock Hg <tcath2s@icloud.com>
2025-11-24 15:23:38 +0800

    feat: generate certs and enable ssl for nginx

---
+++ certs/gen.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+
+############################### sign CA ###############################
+# Generate a 4096-bit RSA private key and encrypt it with a passphrase
+openssl genrsa -aes256 -out ca.key 4096
+
+# Create the Certificate Signing Request (CSR)
+openssl req -new -key ca.key -out ca.csr
+
+# Self-sign the CSR to create the Root Certificate
+openssl x509 -req -days 3650 -sha256 -extensions v3_ca -in ca.csr -signkey ca.key -out ca.crt
+
+################################ sign cert by CA ############################
+# Generate a new 2048-bit RSA private key (can omit -aes256 if you don't need encryption)
+openssl genrsa -out server.key 2048
+openssl req -new -key server.key -out server.csr
+openssl x509 -req -days 365 -sha256 \
+    -in server.csr \
+    -CA ca.crt \
+    -CAkey ca.key \
+    -CAcreateserial \
+    -extfile server_ext.cnf \
+    -extensions server_cert \
+    -out server.crt
