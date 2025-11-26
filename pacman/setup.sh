122ee4d785945bba4d6ce6ff1e9c415e8f0cafea
Shylock Hg <tcath2s@icloud.com>
2025-11-26 19:47:36 +0800

    feat: install podman-compose

--- pacman/setup.sh
+++ pacman/setup.sh
@@ -5,7 +5,7 @@ sudo pacman -Syu
 sudo pacman -S --noconfirm --needed \
     code yay mold ninja make cmake gcc clang lldb \
     libc++ libc++abi \
-    ripgrep podman vim emacs stow \
+    ripgrep vim emacs stow \
     rclone inotify-tools \
     flatpak nodejs \
     fcitx5 fcitx5-configtool fcitx5-chinese-addons fcitx5-rime \
@@ -15,7 +15,7 @@ sudo pacman -S --noconfirm --needed \
     wine wine-mono winetricks \
     cronie powertop typst starship \
     hunspell hunspell-en_us \
-    docker docker-compose \
+    docker docker-compose podman podman-compose \
     nginx nginx-mod-stream \
     qemu-full libvirt virt-manager virt-viewer dnsmasq vde2 bridge-utils
 
