d402b577cc23a30239f10b7136c690637c195d2a
Shylock Hg <tcath2s@icloud.com>
2025-11-29 10:27:54 +0800

    minor: comment libvirt network NAT

--- pacman/setup.sh
+++ pacman/setup.sh
@@ -20,8 +20,7 @@ sudo pacman -S --noconfirm --needed \
     qemu-full libvirt virt-manager virt-viewer dnsmasq vde2 bridge-utils
 
 # Append firewall_backend = "iptables" to /etc/libvirt/network.conf resolve the network connection
-# of NAT network problem of guest
-
+# of NAT network problem of guesthk
 yay -S microsoft-edge-stable-bin \
     pgyvisitor \
     tinymist
