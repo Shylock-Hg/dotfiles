1aa1ce43bcf28b1c65604882342e690402ab1f82
Shylock Hg <tcath2s@icloud.com>
2025-12-06 11:44:58 +0800

    feat: set wine app resolution to hdpi

--- wine/setup.sh
+++ wine/setup.sh
@@ -3,10 +3,12 @@
 # winetricks install all cjk fonts
 winetricks cjkfonts
 
+
+sed -i '/"LogPixels"/c\"LogPixels"=dword:000000c0' ~/.wine/user.reg
+
 # WXWorks
 wget https://dldir1.qq.com/wework/work_weixin/WeCom_5.0.0.8002.exe -O ~/Downloads/WeCom_5.0.0.8002.exe
 wine ~/Downloads/WeCom_5.0.0.8002.exe
 
 readonly SH_P=~/.wine/drive_c/users/$USER/Documents
 rm -rf $SH_P  && mkdir $SH_P
-
