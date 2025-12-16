f5f6c594f5145970e5f3b3f74ebf4e23ce15922c
Shylock Hg <tcath2s@icloud.com>
2025-12-16 20:01:55 +0800

    feat: avoid encrypt overwrite error.

--- shylock/sh/backup-home.sh
+++ shylock/sh/backup-home.sh
@@ -3,6 +3,7 @@
 pushd ~
 
 tar --zst -cf ~/Data/shylock.tar.zst Tools Documents Pictures Videos Music
+rm -f ~/Data/shylock.tar.zst.gpg
 gpg2 --encrypt --recipient 'tcath2s@icloud.com' ~/Data/shylock.tar.zst
 
 popd
--- shylock/sh/encrypt-gitlab-backup.sh
+++ shylock/sh/encrypt-gitlab-backup.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
 
+rm -f ~/Data/gitlab.tar.zst
 gpg2 --encrypt --recipient 'tcath2s@icloud.com' ~/Data/gitlab.tar.zst
