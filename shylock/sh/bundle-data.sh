949f506803c58db6e65f73b5be62bd409fd31be1
Shylock Hg <tcath2s@icloud.com>
2025-12-21 18:50:35 +0800

    minor: bundle in one command

--- shylock/sh/bundle-data.sh
+++ shylock/sh/bundle-data.sh
@@ -1,8 +1,20 @@
 #! /usr/bin/env bash
 
+set -ex
+
+readonly SCRIPT_DIR=$(dirname $0)
+
+$SCRIPT_DIR/encrypt-gitlab-backup.sh
+$SCRIPT_DIR/backup-home.sh
+
+cp -r $SCRIPT_DIR/../../gpg ~/Data
+
 pushd ~/Data
 
 tar -cf bundle.tar gitlab.tar.zst.gpg  shylock.tar.zst.gpg
-mkisofs -J -r -V "MY_DATA_BUNDLE" -o bundle.iso bundle.tar
+mkisofs -J -r -V "MY_DATA_BUNDLE" -o bundle.iso bundle.tar gpg
+
+# cleanup
+rm -rf ./gitlab.* ./shylock.* ./gpg
 
 popd
