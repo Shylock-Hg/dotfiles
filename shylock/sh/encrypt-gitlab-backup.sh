34488081a33e9308a65283fad47f6df5bda03cff
Shylock Hg <tcath2s@icloud.com>
2025-12-28 14:50:46 +0800

    feat: add subkey.

--- shylock/sh/encrypt-gitlab-backup.sh
+++ shylock/sh/encrypt-gitlab-backup.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
 rm -f ~/Data/gitlab.tar.zst.gpg
-gpg2 --encrypt --recipient 'tcath2s@icloud.com' ~/Data/gitlab.tar.zst
+gpg2 --encrypt --recipient 'E63EE5A890228C2A' ~/Data/gitlab.tar.zst
--- shylock/sh/gpg-b64.sh
+++ shylock/sh/gpg-b64.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-gpg2 --encrypt --recipient 'tcath2s@icloud.com' $1
+gpg2 --encrypt --recipient 'E63EE5A890228C2A' $1
 base64 $1.gpg > $1.gpg.b64
 
 rm $1.gpg
