76522caf4695c0995a0766f4622813fe0c4fc8b5
Shylock Hg <tcath2s@icloud.com>
2026-01-18 09:43:11 +0800

    feat: print profit percentage.

--- utils/rate.py
+++ utils/rate.py
@@ -5,12 +5,15 @@ import argparse
 def rate(lo, hi, ratio):
     return lo + (hi - lo) * ratio
 
+def earn(lo, sell):
+    return (sell - lo) / lo * 100
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("lo", type=float)
     parser.add_argument("hi", type=float)
     parser.add_argument("--ratio", type=float)
+    parser.add_argument("--amount", type=int)
     parser.add_argument("--reverse", action="store_true")
 
     parser.add_argument('--type', type=str)
@@ -25,10 +28,11 @@ if __name__ == "__main__":
         if args.reverse:
             assert(ratio <= 1)
             ratio = 1-ratio
-        print("Range value: {}", round(rate(args.lo, args.hi, ratio), 2))
+        sell = round(rate(args.lo, args.hi, ratio), 2)
+        print("Range value: {}", sell)
+        print("Earn: ", earn(args.lo, sell), "%")
     elif args.type == 'down':
         ratio = args.ratio if args.ratio is not None else 0.0618
         sell = round(args.hi - (args.hi * ratio), 2)
         print("Down value: {}", sell)
-        print("Earn: ", (sell - args.lo) / args.lo * 100, "%")
-        
+        print("Earn: ", earn(args.lo, sell), "%")
