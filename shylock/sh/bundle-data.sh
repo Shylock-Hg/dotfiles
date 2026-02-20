238c4bd2302c3e3a37fe8d5389a494f8c9e120c9
Shylock Hg <tcath2s@icloud.com>
2026-02-20 10:47:29 +0800

    minor: remove bundle elpa data.

--- shylock/sh/bundle-data.sh
+++ shylock/sh/bundle-data.sh
@@ -15,6 +15,6 @@ tar -cf bundle.tar gitlab.tar.zst.gpg  shylock.tar.zst.gpg elpa-mirror-*
 mkisofs -J -r -V "MY_DATA_BUNDLE" -o bundle.iso bundle.tar gpg
 
 # cleanup
-rm -rf ./gitlab.* ./shylock.* ./gpg
+rm -rf ./gitlab.* ./shylock.* ./gpg elpa-mirror-*
 
 popd
