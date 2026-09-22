3546d42c530d791a0278d93a3f965dac6fb67ae2
Shylock Hg <tcath2s@icloud.com>
2026-09-22 10:59:31 +0800

    feat: upaload to gdrive.

--- shylock/sh/upload-backup.sh
+++ shylock/sh/upload-backup.sh
@@ -2,4 +2,6 @@
 
 readonly TARBALL=bundle.tar
 
-rclone sync ~/Data/$TARBALL dropbox:/$TARBALL --transfers 4
+#rclone sync ~/Data/$TARBALL dropbox:/$TARBALL --transfers 4
+
+rclone sync ~/Data/$TARBALL gdrive:/$TARBALL --transfers 4
