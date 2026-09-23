41ad70f1e265578dbd0cf3185cce6e5a0a8641d1
Shylock Hg <tcath2s@icloud.com>
2026-09-23 15:18:25 +0800

    feat: bundle forgejo dump

--- shylock/sh/bundle-data.sh
+++ shylock/sh/bundle-data.sh
@@ -4,7 +4,7 @@ set -ex
 
 readonly SCRIPT_DIR=$(dirname $0)
 
-$SCRIPT_DIR/encrypt-gitlab-backup.sh
+$SCRIPT_DIR/encrypt-forgejo-backup.sh
 $SCRIPT_DIR/backup-home.sh
 $SCRIPT_DIR/backup-elpa-mirror.sh
 
@@ -12,10 +12,10 @@ cp -r $SCRIPT_DIR/../../gpg ~/Data
 
 pushd ~/Data
 
-tar -cf bundle.tar gitlab.tar.zst.gpg  shylock.tar.zst.gpg elpa-mirror-*
+tar -cf bundle.tar forgejo.tar.zst.gpg  shylock.tar.zst.gpg elpa-mirror-*
 mkisofs -J -r -V "MY_DATA_BUNDLE" -o bundle.iso bundle.tar gpg
 
 # cleanup
-rm -rf ./gitlab.* ./shylock.* ./gpg elpa-mirror-*
+rm -rf ./gitlab.* ./shylock.* ./gpg elpa-mirror-* ./forgejo.*
 
 popd
