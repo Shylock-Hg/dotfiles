ee792a6ccce24de6ee0008f84dd413a754306e38
Shylock Hg <tcath2s@icloud.com>
2026-09-27 00:29:16 +0800

    fix: dnsmasq with headscale.

--- dnsmasq/setup.sh
+++ dnsmasq/setup.sh
@@ -6,3 +6,8 @@ sudo mkdir -p /etc/dnsmasq.d
 sudo cp $SCRIPT_DIR/tailscale.conf /etc/dnsmasq.d/tailscale.conf
 
 echo 'conf-dir=/etc/dnsmasq.d/,*.conf' | sudo tee -a /etc/dnsmasq.conf
+
+# Route shylockhg.me to dnsmasq on the local machine as well
+sudo mkdir -p /etc/systemd/resolved.conf.d
+sudo cp $SCRIPT_DIR/resolved-forgejo.conf /etc/systemd/resolved.conf.d/forgejo.conf
+sudo systemctl restart systemd-resolved
